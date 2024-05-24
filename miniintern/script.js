42663be4dc09506f787e701ddb83874d972416fe
qpwoei0123 <qpwoei0123@naver.com>
2024-05-24 18:08:47 +0900

    미니인턴 <h> 스크롤이벤트 추가

--- miniintern/script.js
+++ miniintern/script.js
@@ -6,7 +6,7 @@ window.onload = function () {
           trigger: el,
           start: "0% 100%",
           end: "0% 100%",
-          scrub: 1,
+          scrub: 2,
         },
       })
       .fromTo(
@@ -16,4 +16,56 @@ window.onload = function () {
         0
       );
   });
+
+  gsap.utils.toArray("h2").forEach((el) => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: el,
+          start: "0% 100%",
+          end: "0% 100%",
+          scrub: 2,
+        },
+      })
+      .fromTo(
+        el,
+        { opacity: 0, x: 100 },
+        { opacity: 1, x: 0, ease: "none", duration: 1 },
+        0
+      );
+  });
+  gsap.utils.toArray("h4").forEach((el) => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: el,
+          start: "0% 100%",
+          end: "0% 100%",
+          scrub: 2,
+        },
+      })
+      .fromTo(
+        el,
+        { opacity: 0, x: 100 },
+        { opacity: 1, x: 0, ease: "none", duration: 1 },
+        0
+      );
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#section-one",
+        start: "100% 70%",
+        end: "100% 0%",
+        scrub: 2,
+      },
+    })
+    .to(
+      ".main-img-2",
+      { x: 50, y: -250, rotate: -30, ease: "none", duration: 5 },
+      0
+    );
 };
