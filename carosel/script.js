9a66d2b06ea2b227699c1f019a2068af49eed69a
qpwoei0123 <qpwoei0123@naver.com>
2024-05-29 14:57:49 +0900

    캐러셀 리팩토링

--- carosel/script.js
+++ carosel/script.js
@@ -9,7 +9,6 @@
 
   const $slideContainer = get(".slider__wrapper");
   const $slider = get(".slider");
-
   const $totalSlides = get(".all-slide");
   const $currentSlide = get(".current-slide");
   const $prevBtn = get(".control__button.prev");
@@ -19,39 +18,32 @@
   const slideWidth = $slide[0].clientWidth;
   const slideAmount = $slide.length;
   const sliderWidth = slideWidth * slideAmount;
-  const slideSpeed = 1000;
+  const slideSpeed = 1 * 1000;
 
   let currentIndex = 1;
   let moveOffset = 0;
 
+  // autoPlay 관련
   let interval;
+  const intervalSecond = 2 * 1000;
 
   // setInterval을 사용, 3초마다 handleSwipe를 트리거하는 자동 재생 함수
   const slideAutoPlay = () => {
     interval = setInterval(() => {
       handleSwipe(1);
-      if (currentIndex === $slider.children.length - 1) {
-        setTimeout(() => {
-          $slider.style.transition = "none";
-          currentIndex = 1;
-          moveOffset = (100 / slideAmount) * currentIndex;
-          $slider.style.transform = `translateX(-${moveOffset}%)`;
-        }, slideSpeed);
-      }
-    }, 2000);
+    }, intervalSecond);
   };
 
+  // direction이 양수이면 다음 슬라이드로, 음수이면 이전 슬라이드로 이동
   const handleSwipe = (direction) => {
-    currentIndex = currentIndex + direction;
+    direction > 0 ? currentIndex++ : currentIndex--;
 
-    if (currentIndex >= slideAmount + 2) {
-      currentIndex = 4;
-    } else if (currentIndex <= 0) {
-      currentIndex = 0;
-    }
-
+    // 슬라이드 이동 효과
     moveOffset = (100 / slideAmount) * currentIndex;
+    $slider.style.transform = `translateX(-${moveOffset}%)`;
+    $slider.style.transition = `transform ${slideSpeed}ms ease`;
 
+    // currentSlide.textContent를 미리 갱신
     if (currentIndex <= 0) {
       $currentSlide.textContent =
         $slider.children[$slider.children.length - 2].dataset.index;
@@ -61,62 +53,30 @@
       $currentSlide.textContent = $slider.children[currentIndex].dataset.index;
     }
 
-    $slider.style.transform = `translateX(-${moveOffset}%)`;
-    $slider.style.transition = `all ${slideSpeed}ms ease`;
-  };
-
-  // 이전, 다음 버튼 클릭 이벤트 핸들러
-  // handleSwipe 함수를 호출하면서 슬라이드의 진행 방향을 인자로 넘겨주는 함수
-  const handleMoveBtn = (event) => {
-    event.preventDefault();
-    const $target = event.currentTarget;
-
-    if ($target.id === "nextBtn") {
-      handleSwipe(1);
-
-      if (currentIndex === $slider.children.length - 1) {
-        setTimeout(() => {
-          $slider.style.transition = "none";
-          currentIndex = 1;
-          moveOffset = (100 / slideAmount) * currentIndex;
-          $slider.style.transform = `translateX(-${moveOffset}%)`;
-        }, slideSpeed);
-      }
-    } else {
-      handleSwipe(-1);
-
-      if (currentIndex === 0) {
-        setTimeout(() => {
-          $slider.style.transition = "none";
-          currentIndex = $slider.children.length - 2;
-          moveOffset = (100 / slideAmount) * currentIndex;
-          $slider.style.transform = `translateX(-${moveOffset}%)`;
-        }, slideSpeed);
-      }
+    // 슬라이드의 인덱스가 0이거나 마지막 슬라이드의 인덱스일 때 (클론 슬라이드일 때)
+    // transition이 끝나면, transition을 제거하고, currentIndex를 조정하여 무한 슬라이드 효과를 준다.
+    // 이때, transition을 제거하지 않으면, 슬라이드의 이동과정이 보이기 떄문에 무한 슬라이드 느낌이 사라진다.
+    if (currentIndex === $slider.children.length - 1 || currentIndex === 0) {
+      setTimeout(() => {
+        $slider.style.transition = "none";
+        currentIndex = currentIndex === 0 ? $slider.children.length - 2 : 1;
+        moveOffset = (100 / slideAmount) * currentIndex;
+        $slider.style.transform = `translateX(-${moveOffset}%)`;
+      }, slideSpeed);
     }
   };
 
-  // 슬라이드의 초기 레이아웃 SetUp
-  const setSlideLayout = () => {
-    // 무한 슬라이드를 위해 첫번째 슬라이드와 마지막 슬라이드를 복제
-    const $firstSlideClone = $slider.firstElementChild.cloneNode(true);
-    const $lastSlideClone = $slider.lastElementChild.cloneNode(true);
-
-    $slider.insertBefore($lastSlideClone, $slider.firstElementChild);
-    $slider.appendChild($firstSlideClone);
-
-    // 슬라이드의 너비를 설정하고, 1번째 슬라이드로 위치 초기화 (translateX)
-    $slider.style.width = `${sliderWidth}px`;
-    $slider.style.transform = `translateX(-${slideWidth}px)`;
+  // 이전, 다음 버튼 클릭 이벤트 핸들러
+  const handleMoveBtn = (event) => {
+    const $target = event.currentTarget;
+    const direction = $target.classList.contains("next") ? 1 : -1;
+    handleSwipe(direction);
   };
 
-  const handleMouseEnter = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
+  const handleMouseEnter = () => {
     clearInterval(interval);
   };
-
-  const handleMouseLeave = (e) => {
+  const handleMouseLeave = () => {
     slideAutoPlay();
   };
 
@@ -134,17 +94,25 @@
     };
   };
 
-  const handleMoveBtnThrottled = throttle(handleMoveBtn, 1000);
-
   const init = () => {
-    setSlideLayout();
+    // 무한 슬라이드를 위해 첫번째 슬라이드와 마지막 슬라이드를 복제
+    const $firstSlideClone = $slider.firstElementChild.cloneNode(true);
+    const $lastSlideClone = $slider.lastElementChild.cloneNode(true);
+
+    $slider.insertBefore($lastSlideClone, $slider.firstElementChild);
+    $slider.appendChild($firstSlideClone);
+
+    // 슬라이드 추가로 인해 슬라이더의 너비가 변경되었으므로 다시 계산
+    $slider.style.width = `${sliderWidth}px`;
+    $slider.style.transform = `translateX(-${slideWidth}px)`;
+
     slideAutoPlay();
 
     $totalSlides.textContent = slideAmount;
     $currentSlide.textContent = currentIndex;
 
-    $prevBtn.addEventListener("click", handleMoveBtnThrottled);
-    $nextBtn.addEventListener("click", handleMoveBtnThrottled);
+    $prevBtn.addEventListener("click", throttle(handleMoveBtn, slideSpeed));
+    $nextBtn.addEventListener("click", throttle(handleMoveBtn, slideSpeed));
 
     $slideContainer.addEventListener("mouseenter", handleMouseEnter);
     $slideContainer.addEventListener("mouseleave", handleMouseLeave);
